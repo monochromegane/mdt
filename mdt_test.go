f675c7c6b0415dc88499e8a10dab557de39c6a8c
monochromegane <dev.kuro.obi@gmail.com>
2015-05-16 19:21:40 +0900

    Added examples.

---
+++ mdt_test.go
@@ -0,0 +1,95 @@
+package mdt_test
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/monochromegane/mdt"
+)
+
+func ExampleConvert_csv() {
+	r := strings.NewReader(`
+headerA, headerB
+content, content
+`)
+	result, _ := mdt.Convert(r)
+	fmt.Printf("%s", result)
+
+	// Output:
+	// | headerA | headerB |
+	// | ------- | ------- |
+	// | content | content |
+}
+
+func ExampleConvert_tsv() {
+	r := strings.NewReader(`
+headerA	headerB
+content	content
+`)
+	result, _ := mdt.Convert(r)
+	fmt.Printf("%s", result)
+
+	// Output:
+	// | headerA | headerB |
+	// | ------- | ------- |
+	// | content | content |
+}
+
+func ExampleConvert_format() {
+	r := strings.NewReader(`
+headerA, headerB
+short, very very long content
+`)
+	result, _ := mdt.Convert(r)
+	fmt.Printf("%s", result)
+
+	// Output:
+	// | headerA | headerB                |
+	// | ------- | ---------------------- |
+	// | short   | very very long content |
+}
+
+func ExampleConvert_multibyte() {
+	r := strings.NewReader(`
+headerA, headerB
+マルチバイト文字, content
+`)
+	result, _ := mdt.Convert(r)
+	fmt.Printf("%s", result)
+
+	// Output:
+	// | headerA          | headerB |
+	// | ---------------- | ------- |
+	// | マルチバイト文字 | content |
+}
+
+func ExampleConvert_align() {
+	r := strings.NewReader(`
+headerA:, :headerB:
+content, content
+`)
+	result, _ := mdt.Convert(r)
+	fmt.Printf("%s", result)
+
+	// Output:
+	// | headerA | headerB |
+	// | -------:|:-------:|
+	// | content | content |
+}
+
+func ExampleConvert_repeat() {
+	r := strings.NewReader(`
+| headerA | headerB |
+| -------:|:-------:|
+| content | content |
+next content, next content
+`)
+	result, _ := mdt.Convert(r)
+	fmt.Printf("%s", result)
+
+	// Output:
+	// | headerA      | headerB      |
+	// | ------------:|:------------:|
+	// | content      | content      |
+	// | next content | next content |
+}
